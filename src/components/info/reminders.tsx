830a63521001e234f5111ac3987e50a6eaa38395
Davis Ford <davis.e.ford@gmail.com>
2019-08-15 22:30:20 -0400

    Adding boilerplate

--- src/components/info/reminders.tsx
+++ src/components/info/reminders.tsx
@@ -89,6 +89,7 @@ const getTitle = ({
   name,
   scenery,
   spell,
+  triumph,
 }: TTurnAction): string => {
   const suffix = name === condition ? `` : `: ${condition}`
   if (artifact) return `Artifact${suffix}`
@@ -96,6 +97,7 @@ const getTitle = ({
   if (endless_spell) return `Endless Spell${suffix}`
   if (scenery) return `Scenery${suffix}`
   if (spell) return `Spell${suffix}`
+  if (triumph) return `Triumph${suffix}`
   return condition
 }
 
